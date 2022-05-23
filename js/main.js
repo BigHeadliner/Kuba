df4fc4b4842e87da32ac8ca4e6241a9f0bd02cc3
BigHeadliner <bigheadliner@gmail.com>
2022-05-23 18:25:10 +0300

    about section adaptive ready

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 $(function(){
   $('.photo__slider').slick({ 
       prevArrow:'<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>', 
-      nextArrow:'<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
+      nextArrow:'<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>', 
+      responsive: [
+        {
+          breakpoint: 700,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            arrows:false, 
+            dots: true, 
+          }
+        },
+      ]
   }) 
   $('.menu__btn').on('click', function(){ 
-    $('.menu__list').slideToggle();
+    $('.menu__list').slideToggle(); 
   });
 });
